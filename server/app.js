81c8953c28c21b6c86f4cf787a0155c8c7b830b4
NidhiChauhan92 <ndhpanchal1034@gmail.com>
2023-12-29 22:52:11 +0530

    some changes in cors

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const loginRoutes = require("./routes/login");
 const orderRoutes = require("./routes/order");
 const SECRET = "restapi";
 const bodyparser = require("body-parser");
-const cors = require('cors')
+// const cors = require('cors')
 var jwt = require('jsonwebtoken');
 const User = require("./models/register");
 
@@ -21,7 +21,7 @@ app.use((req,res,next)=>{
 })
 
 
-app.use(cors());
+// app.use(cors());
   // {
   //   origin: '*',
   //   methods: '*',
